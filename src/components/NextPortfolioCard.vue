f0ce7fe1f17f63854398a8c08a39a3af571ee342
je <qwer90218@gmail.com>
2023-09-12 18:56:17 +0800

    update photo

--- src/components/NextPortfolioCard.vue
+++ src/components/NextPortfolioCard.vue
@@ -4,7 +4,7 @@
       <p class="text-xl mb-2">next portfolio</p>
       <p class="text-4xl mb-8">name</p>
       <div class="bg-red-900">
-        <img src="../assets/media/personal_photos.jpg" class="w-56 h-72 object-cover mb-10" alt="">    
+        <img src="../assets/media/personal_photos2.jpg" class="w-56 h-72 object-cover mb-10" alt="">    
       </div> 
     </div>
   </a>
