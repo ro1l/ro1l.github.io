0188fe140d22db0555f1e569e6398129e2ab8b3d
je <qwer90218@gmail.com>
2023-09-12 18:49:27 +0800

    調整照片位址

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,7 @@
 <template>
   <section id="about" class="w-100 h-screen
     flex justify-center items-center">
-    <div class="
-      flex">
+    <div class="flex">
       <div class="w-6/12
         flex flex-col">
         <p class="mb-10 text-lg leading-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi consequuntur totam saepe adipisci ex expedita at laudantium perspiciatis, ipsum ad tempora amet rem quo iure a ratione accusamus sit perferendis?</p>
@@ -12,7 +11,7 @@
 
       <div class="w-6/12 h-100">
         <div class="flex justify-center items-center">
-          <img class="object-cover h-56 w-4/12" src="../assets/media/personal_photos2.jpg" alt="">
+          <img class="object-cover h-56 w-4/12" src="../assets/media/personal_photos.jpg" alt="">
         </div>
       </div>
     </div>
