71fc13439a0431bd345dbab144bf3751d3cd2e41
je <qwer90218@gmail.com>
2023-09-12 18:50:04 +0800

    新增點擊前往路由、渲染資料

--- src/views/PortfoliosView.vue
+++ src/views/PortfoliosView.vue
@@ -1,21 +1,25 @@
 <script setup>
-// import { ref } from 'vue';
   import { useRouter } from 'vue-router';
 
   const router = useRouter();
 
-  function getPortfolio(id) {
-    router.push(`${id}`);
+  function getPortfolio(name) {
+    router.push(`/portfolio/${name}`);
   }
 
+  import { useDataStore } from '../stores/dataStore';
+
+  const store = useDataStore();
 </script>
 
 <template>
   <section id="portfolios" class="w-100 py-72">
     <div class="columns-1 w-3/12 m-auto">
-      <img src="../assets/media/personal_photos2.jpg" class=" h-72 object-cover mb-10" alt=""
-        @click.prevent="getPortfolio(123)">
-      <img src="../assets/media/personal_photos2.jpg" class=" h-72 object-cover mb-10" alt="">
+      <template v-for="portfolio in store.portfolios" :key="portfolio">
+        <p class="text-sm mb-1.5 font-light">{{ portfolio.name }}</p>
+        <img :src="portfolio.imgMain" class="h-72 object-cover mb-16 cursor-pointer" alt=""
+          @click.prevent="getPortfolio(portfolio.name)">
+      </template>
     </div>
   </section>
 </template>
