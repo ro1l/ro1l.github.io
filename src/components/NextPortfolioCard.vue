8643d8668c0e36b2ee977fcd3c76d13a1fd51f06
je <qwer90218@gmail.com>
2023-09-12 18:48:05 +0800

    下一個作品展示元件

---
+++ src/components/NextPortfolioCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <a href="">
+    <div class="mt-20">
+      <p class="text-xl mb-2">next portfolio</p>
+      <p class="text-4xl mb-8">name</p>
+      <div class="bg-red-900">
+        <img src="../assets/media/personal_photos.jpg" class="w-56 h-72 object-cover mb-10" alt="">    
+      </div> 
+    </div>
+  </a>
+</template>
