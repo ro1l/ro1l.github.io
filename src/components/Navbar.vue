959de7b87831c950e7d61530d1ccceae6ae25ad7
je <qwer90218@gmail.com>
2023-09-12 18:47:45 +0800

    拉出navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<script setup>
+  const props = defineProps({
+    isAboutActive: {
+      type: Boolean,
+      default: false
+    },
+    isSkillsActive: {
+      type: Boolean,
+      default: false
+    },
+    isPortfoliosActive: {
+      type: Boolean,
+      default: false
+    },
+    isContactActive: {
+      type: Boolean,
+      default: false
+    },
+  });
+</script>
+
+<template>
+  <nav class="px-12 py-24
+  flex flex-col
+  fixed bottom-0 left-0">
+
+    <router-link to="/#about" class="mb-3" :class="{ 'py-3 text-3xl': props.isAboutActive }">
+      about
+    </router-link>
+
+    <router-link to="/#skills" class="mb-3 " :class="{ 'py-3 text-3xl': isSkillsActive }">
+      skills
+    </router-link>
+
+    <router-link to="/#portfolios" class="mb-3 " :class="{ 'py-3 text-3xl': isPortfoliosActive }">
+      portfolios
+    </router-link>
+  
+    <router-link to="/#contact" class="" :class="{ 'py-3 text-3xl': isContactActive }">
+      contact
+    </router-link>
+
+  </nav>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup>
-import TopBar from '../components/TopBar.vue';
 import BannerView from './BannerView.vue';
 import AboutView from './AboutView.vue';
 import SkillsView from './SkillsView.vue';
 import PortfoliosView from './PortfoliosView.vue';
 import ContactView from './ContactView.vue';
+import Navbar from '../components/Navbar.vue';
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 
 function useElementVisibility(elementId) {
@@ -33,27 +33,16 @@ const isAboutActive = useElementVisibility('about');
 const isSkillsActive = useElementVisibility('skills');
 const isPortfoliosActive = useElementVisibility('portfolios');
 const isContactActive = useElementVisibility('contact');
+
+
 </script>
 
 <template>
-  <TopBar />
-
-  <nav class="px-12 py-24
-    flex flex-col
-    fixed bottom-0 left-0">
-    <router-link to="/#about" class="mb-3" :class="{ 'py-3 text-3xl': isAboutActive }">
-      about
-    </router-link>
-    <router-link to="/#skills" class="mb-3" :class="{ 'py-3 text-3xl': isSkillsActive }">
-      skills
-    </router-link>
-    <router-link to="/#portfolios" class="mb-3" :class="{ 'py-3 text-3xl': isPortfoliosActive }">
-      portfolios
-    </router-link>
-    <router-link to="/#contact" class="" :class="{ 'py-3 text-3xl': isContactActive }">
-      contact
-    </router-link>
-  </nav>
+  <Navbar
+  :isAboutActive = isAboutActive
+  :isSkillsActive = isSkillsActive
+  :isPortfoliosActive = isPortfoliosActive
+  :isContactActive = isContactActive />
 
   <div class="container w-8/12 ml-auto mr-24">
     <BannerView />
