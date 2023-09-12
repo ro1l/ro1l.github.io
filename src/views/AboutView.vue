f7ee2a9beb19d6eb5183a119e0fa4828047f4e79
je <qwer90218@gmail.com>
2023-09-12 21:22:07 +0800

    test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,24 @@
+<script setup>
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { ref, onMounted } from "vue";
+gsap.registerPlugin(ScrollTrigger);
+
+const box = ref(null);
+
+onMounted(() => {
+  gsap.to(box.value, {
+    x: 200,
+    scrollTrigger: {
+      trigger: box.value, // 正确指定触发器元素
+      start: "10% left", // 触发器起始位置
+      end: "start right", // 触发器结束位置
+      scrub: true // 是否随着滚动缓动
+    }
+  });
+});
+</script>
+
 <template>
   <section id="about" class="w-100 h-screen
     flex justify-center items-center">
@@ -16,4 +37,6 @@
       </div>
     </div>
   </section>
+  <div class="w-10 h-10 bg-green-500" ref="box"></div>
+
 </template>
