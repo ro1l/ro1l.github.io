f3f01a39b2e44acb67695bfa7456462bc2ddea37
je <qwer90218@gmail.com>
2023-09-16 08:40:33 +0800

    change content

--- src/views/BannerView.vue
+++ src/views/BannerView.vue
@@ -5,7 +5,7 @@
     <p class="text-3xl leading-relaxed
     lg:text-7xl lg:leading-snug">
       嗨，我是蔡潔儀，<br>
-      正在努力追求成為一名<br>
+      正在努力成為一名<br>
       優秀的前端工程師。<br>
       目前使用的框架是 Vue.js，<br>
       計劃在未來擴展更多技能。
